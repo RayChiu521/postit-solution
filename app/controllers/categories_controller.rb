b50d96d7b573fef6906a8494cec6771aa9e86637
Ray <huihong521@gmail.com>
2013-11-09 14:28:25 +0800

    Add the new/create action of Category.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,18 @@
+class CategoriesController < ApplicationController
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(params.require(:category).permit(:name))
+
+		if @category.save
+			flash[:notice] = "Category was created"
+			redirect_to posts_path
+		else
+			render "new"
+		end
+	end
+
+end
