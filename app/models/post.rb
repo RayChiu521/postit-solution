188044f650af81c61dcf0571a2a97913ffaddac2
Ray <huihong521@gmail.com>
2013-11-09 14:12:22 +0800

    Add the Post feature.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	has_many :post_categories
 	has_many :categories, :through => :post_categories, dependent: :destroy
+
+	validates :title, presence: true, length: {minimum: 5}
+	validates :description, presence: true
+	validates :url, presence: true, uniqueness: true
 end
