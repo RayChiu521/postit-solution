c6a5fa1c7871182aa6dc1672b4567118e30fa93e
Ray <huihong521@gmail.com>
2013-11-15 22:09:41 +0800

    Fix the vote polymorphic uniqueness.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,7 +3,7 @@ class Vote < ActiveRecord::Base
 	belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 	belongs_to :voteable, polymorphic: true
 
-	validates :creator, uniqueness: { scope: :voteable }
+	validates :creator, uniqueness: { scope: [:voteable_type, :voteable_id] }
 
 
 	def self.total_votes (obj)
