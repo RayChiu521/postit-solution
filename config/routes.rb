61c2f5fc17443ddb87fea1b6b8d2bf5eb8458fe3
Ray <huihong521@gmail.com>
2013-11-16 08:29:28 +0800

    Display all posts and comments the user created.

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,15 @@ PostitTemplate::Application.routes.draw do
   	member do
   		post '/vote', to: 'posts#vote'
   	end
-  	resources :comments, only: [:create] do
-      member do
-        post '/vote', to: 'comments#vote'
-      end
+  	resources :comments, only: [:create]
+  end
+
+  resources :comments, only: [] do
+    member do
+      post '/vote', to: 'comments#vote'
     end
   end
+
   resources :categories, only: [:show, :new, :create]
   resources :users, except: [:index]
 end
