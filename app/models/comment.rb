075ad5def328bd5f79ae969c087d6bdbc69a4f0f
Ray <huihong521@gmail.com>
2013-11-20 21:48:38 +0800

    Extract voteable to lib.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,12 @@
 class Comment < ActiveRecord::Base
+
+	include Voteable
+
 	belongs_to :creator, class_name: "User", foreign_key: "user_id"
 	belongs_to :post
 	has_many :votes, as: :voteable
 
-	scope :list, -> { all.sort_by{ |x| Vote.total_votes(x) }.reverse }
+	scope :list, -> { all.sort_by{ |x| x.total_votes }.reverse }
 
 	validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,14 @@
 class Post < ActiveRecord::Base
+
+	include Voteable
+
 	belongs_to :creator, class_name: "User", foreign_key: "user_id"
 	has_many :comments, dependent: :destroy
 	has_many :post_categories
 	has_many :categories, :through => :post_categories, dependent: :destroy
 	has_many :votes, as: :voteable
 
-	scope :list, -> { all.sort_by{ |x| Vote.total_votes(x) }.reverse }
+	scope :list, -> { all.sort_by{ |x| x.total_votes }.reverse }
 
 	validates :title, presence: true, length: {minimum: 5}
 	validates :description, presence: true
